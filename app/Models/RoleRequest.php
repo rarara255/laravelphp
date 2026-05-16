5e40970ba002d8161178de7e7e17899246fa9043
Academy <Academy@gmail.com>
2026-05-16 13:17:33 +0800

    upd

--- app/Models/RoleRequest.php
+++ app/Models/RoleRequest.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class RoleRequest extends Model
 {
     use HasFactory;
+    protected $fillable =['requested_role', 'reason', 'user_id'];
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+
+    public function scopePending($query){
+        return $query->where('status', 'pending');
+    }
 }
