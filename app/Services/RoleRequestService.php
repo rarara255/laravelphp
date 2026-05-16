10c5f95cef44bfe614d68d8b76e09465408c417b
Academy <Academy@gmail.com>
2026-05-16 13:02:59 +0800

    UPD

---
+++ app/Services/RoleRequestService.php
@@ -0,0 +1,17 @@
+<?php
+use App\Models\RoleRequest;
+namespace App\Services;
+
+class RoleRequestService
+{
+    public function create(array $data, int $userId): RoleRequest
+    {
+        $roleRequest = RoleRequest::create(arra_merge($data,
+            ['user_id' => $userId]));
+        return $roleRequest;
+    }
+    public function approveRequest(RoleRequest $roleRequest): void{
+        $roleRequest->update(['status' => 'approved']);
+
+    }
+}
