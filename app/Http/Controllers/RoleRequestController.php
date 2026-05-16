7d000cf49603e25c06b84191b4a107fb1eb01cd9
Academy <Academy@gmail.com>
2026-05-16 12:02:11 +0800

    upd

--- app/Http/Controllers/RoleRequestController.php
+++ app/Http/Controllers/RoleRequestController.php
@@ -14,6 +14,6 @@ class RoleRequestController extends Controller
     protected RoleRequestService $service;
 
     public function __construct(RoleRequestService $service){
-        $this->service = $service
+        $this->service = $service;
     }
 }
