f92b4bb8c68bee5536c6d20ad3096f6f7ac037f9
Academy <Academy@gmail.com>
2026-05-16 11:12:50 +0800

    upd

--- resources/views/role_requests/create.blade.php
+++ resources/views/role_requests/create.blade.php
@@ -7,10 +7,10 @@
         <form action="{{route('role_requests.store')}}" method="POST" class="mt-5">
         @csrf
         <div class="mb-4">
-            <label for="requested_role" class="font-medium text-red-700">Роль:</label>
+            <label for="requested_role" class="font-medium text-red-700 rounded-md block mt-1">Роль:</label>
             <select name="requested_role" id="requested_role" class="border-black-300">
                 <option value="author">Author(creating new Tasks)</option>
-                <option></option>
+                <option value="editor">Editor(edit all Tasks)</option>
             </select>
         </div>
         </form>
