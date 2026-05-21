585806966649fd63b6ac794c0b1584f09f6033d3
Academy <Academy@gmail.com>
2026-05-21 17:06:14 +0800

    123

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -55,6 +55,7 @@ class Kernel extends HttpKernel
      */
     protected $routeMiddleware = [
         //'auth' => \App\Http\Middleware\Authenticate::class,
+        'admin' => \App\Http\Middleware\EnsureUserIsAdmin::class,
         'auth' => \App\Http\Middleware\EnsureIsAuthenticated::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
